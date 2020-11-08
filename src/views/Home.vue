47b132ce0f4e8ef34dd2755c928397957cf4362e
nogrenouille <noemiegrenouille@gmail.com>
2020-11-08 14:30:55 +0100

    Create vue project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>nogrenouille</h1>
+    <p>
+      En attendant l'ouverture prochaine de ce <strong>portfolio</strong>, venez
+      donc me suivre sur
+      <a href="https://instagram.com/nogrenouille">Instagram</a> &
+      <a href="https://twitter.com/nogrenouille">Twitter</a> :)
+    </p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
