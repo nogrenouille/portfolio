6195bdea888181b472258bc292f0a97aa8062dc1
nogrenouille <noemiegrenouille@gmail.com>
2020-11-10 01:00:34 +0100

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/portfoliovue/" : "/"
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "nogrenouille : Illustrations, musique & diableries.";
+      return args;
+    });
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/"
 };
